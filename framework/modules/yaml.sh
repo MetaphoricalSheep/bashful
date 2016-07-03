63940bc3a5691fedfc7d817341e7914d3a889959
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-07-03 17:21:06 +0200

    Added monkeydir check to yaml module

--- framework/modules/yaml.sh
+++ framework/modules/yaml.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ -z "$__MONKEYDIR__" ]]
+then
+    echo -e "\e[31m""ERROR: \e[39m\e[49m You cannot source this file directly. Source monkey-wrench.sh. \e[39m\n"
+    exit 1
+fi
+
 # sourced from <https://gist.github.com/epiloque/8cf512c6d64641bde388>
 # Based on https://gist.github.com/pkuczynski/8665367
 
