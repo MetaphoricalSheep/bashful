8f4fe4f81fd3257f4bbd357405e717d7697b9e06
Pieter-Uys Fourie <pieteruys.fourie@gmail.com>
2016-07-14 13:57:35 +0200

    Created email sending module Created templating module

---
+++ framework/modules/templates.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ -z "$__MONKEYDIR__" ]]
+then
+    echo -e "\e[31m""ERROR: \e[39m\e[49m You cannot source this file directly. Source monkey-wrench.sh. \e[39m\n"
+    exit 1
+fi
+
+. "$__MONKEYDIR__"/modules/submodules/templates.sh
